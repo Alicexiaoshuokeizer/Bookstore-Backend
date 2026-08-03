9ea5cfee16bbcaef0b3ed4085857e37aa7fb6ccb
Alicexiaoshuokeizer <xiaoshuoye@outlook.com>
2026-08-03 16:58:11 +0100

    Feature: add DatabaseException global handling logic

---
+++ src/main/java/com/cfg/BookStoreBackend/exception/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.cfg.BookStoreBackend.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+// indicate this is a component that handler api endpoint exception to spring
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    // catch DatabaseException that occurs in endpoints
+    // response sends 500 internal server error status code
+    // response also sends a general database error message
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<String> handleDatabaseException(DatabaseException e) {
+        return ResponseEntity
+                .internalServerError()
+                .body("An error occurred while accessing the database");
+    }
+
+}
