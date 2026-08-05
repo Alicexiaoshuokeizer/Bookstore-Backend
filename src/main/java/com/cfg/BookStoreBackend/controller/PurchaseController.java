c0afb611264ff10f25b6e1c0f28debfb4922aded
Alicexiaoshuokeizer <xiaoshuoye@outlook.com>
2026-08-05 16:42:01 +0100

    Fix: add @Requestbody and @Valid to makePurchase() endpoint

--- src/main/java/com/cfg/BookStoreBackend/controller/PurchaseController.java
+++ src/main/java/com/cfg/BookStoreBackend/controller/PurchaseController.java
@@ -3,9 +3,11 @@ package com.cfg.BookStoreBackend.controller;
 import com.cfg.BookStoreBackend.model.dto.PurchaseRequestDTO;
 import com.cfg.BookStoreBackend.model.dto.PurchaseResponseDTO;
 import com.cfg.BookStoreBackend.service.PurchaseService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,7 +16,7 @@ public class PurchaseController {
     private final PurchaseService purchaseService;
 
     @PostMapping("/api/purchases")
-    public ResponseEntity<PurchaseResponseDTO> makePurchase(PurchaseRequestDTO requestDTO) {
+    public ResponseEntity<PurchaseResponseDTO> makePurchase(@Valid @RequestBody PurchaseRequestDTO requestDTO) {
         return ResponseEntity
                 .status(201)
                 .body(purchaseService.makePurchase(requestDTO));
