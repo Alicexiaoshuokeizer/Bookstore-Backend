4623f01bde8a8b5784b588ddfc66371372057583
Alicexiaoshuokeizer <xiaoshuoye@outlook.com>
2026-08-09 23:55:17 +0100

    Test: add unit tests for method `returnBook()` and `addBook()` in `BookService`

--- src/test/java/com/cfg/BookStoreBackend/service/BookServiceTest.java
+++ src/test/java/com/cfg/BookStoreBackend/service/BookServiceTest.java
@@ -1,13 +1,22 @@
 package com.cfg.BookStoreBackend.service;
 
 import com.cfg.BookStoreBackend.exception.DatabaseException;
+import com.cfg.BookStoreBackend.exception.DuplicateOperationException;
 import com.cfg.BookStoreBackend.exception.NotFoundException;
 import com.cfg.BookStoreBackend.model.dto.BookDTO;
 import com.cfg.BookStoreBackend.model.dto.BookResponseDTO;
+import com.cfg.BookStoreBackend.model.dto.ReturnBookRequestDTO;
+import com.cfg.BookStoreBackend.model.dto.ReturnBookResponseDTO;
 import com.cfg.BookStoreBackend.model.entity.Book;
+import com.cfg.BookStoreBackend.model.entity.Customer;
+import com.cfg.BookStoreBackend.model.entity.Purchase;
 import com.cfg.BookStoreBackend.model.repository.BookRepository;
+import com.cfg.BookStoreBackend.model.repository.PurchaseRepository;
+import com.cfg.BookStoreBackend.util.PurchaseStatus;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -23,6 +32,9 @@ class BookServiceTest {
     // Creates a mock BookRepository so the test doesn't use a real DB.
     @Mock
     private BookRepository bookRepository;
+
+    @Mock
+    private PurchaseRepository purchaseRepository;
 
     // Creates BookService and injects the mocked repo into it.
     @InjectMocks
@@ -223,6 +235,211 @@ class BookServiceTest {
 
         // Verify
         verify(bookRepository, times(1)).save(any(Book.class));
+    }
+
+
+    // POST test
+    // when valid return book should restock book and change purchase status to PurchaseStatus.RETURN
+    @Test
+    void returnBookShouldRestockBookAndChangePurchaseStatusWhenValid() {
+        // Assign
+        // create returnBook argument
+        ReturnBookRequestDTO request = new ReturnBookRequestDTO();
+        request.setPurchaseId(1L);
+
+        // create book entity the repo returns when findById
+        Book foundBook = new Book();
+        foundBook.setId(10L);
+        foundBook.setStock(95);
+
+        // create purchase entity the repo returns when findById
+        Purchase foundPurchase = new Purchase();
+        foundPurchase.setId(1L);
+        foundPurchase.setQuantity(5);
+        foundPurchase.setBook(foundBook);
+        foundPurchase.setStatus(PurchaseStatus.CONFIRMED);
+
+        // create book entity the repo returns when save() is called
+        Book updatedBook = new Book();
+        updatedBook.setId(10L);
+        updatedBook.setStock(100);
+        updatedBook.setTitle("Title");
+        updatedBook.setAuthor("Author");
+
+        // create customer entity used in the returned purchase entity after save() via purchaseRepository
+        Customer customer = new Customer();
+        customer.setId(100L);
+
+        // create purchase entity the repo returns when save() is called
+        Purchase updatedPurchase = new Purchase();
+        updatedPurchase.setId(1L);
+        updatedPurchase.setQuantity(5);
+        updatedPurchase.setCustomer(customer);
+        updatedPurchase.setBook(foundBook);
+        updatedPurchase.setStatus(PurchaseStatus.RETURN);
+
+        when(purchaseRepository.findById(1L)).thenReturn(Optional.of(foundPurchase));
+        when(purchaseRepository.save(any(Purchase.class))).thenReturn(updatedPurchase);
+        when(bookRepository.findById(10L)).thenReturn(Optional.of(foundBook));
+
+        // Act
+        ReturnBookResponseDTO responseDTO = bookService.returnBook(request);
+
+        // Assert & Verify
+        // capture the arguments that are passed to repo methods
+        // to verify whether business logic is implemented to the argument successfully/correctly
+        ArgumentCaptor<Purchase> purchaseCaptor = ArgumentCaptor.forClass(Purchase.class);
+        verify(purchaseRepository, times(1)).save(purchaseCaptor.capture());
+        verify(bookRepository, times(1)).findById(10L);
+        verify(purchaseRepository, times(1)).findById(1L);
+
+        Purchase purchaseToSave = purchaseCaptor.getValue();
+
+        // book restock to 95(old stock) + 5(purchase return quantity) = 100(new stock)
+        // business logic applies to foundBook, stock increase correctly
+        assertEquals(100, purchaseToSave.getBook().getStock());
+        // response get updated new book stock correctly
+        assertEquals(100,responseDTO.getUpdatedStock());
+        // business logic applies to purchaseToSave, status changed to RETURN
+        assertEquals(PurchaseStatus.RETURN, purchaseToSave.getStatus());
+        // response status changed to RETURN correctly
+        assertEquals(PurchaseStatus.RETURN, responseDTO.getStatus());
+    }
+
+    // POST test
+    // given a non-existing purchase ID, return book should throw NotFoundException
+    @Test
+    void returnBookShouldThrowNotFoundExceptionGivenNonExistingPurchaseID() {
+        // Assign
+        // create returnBook argument
+        ReturnBookRequestDTO request = new ReturnBookRequestDTO();
+        request.setPurchaseId(1L);
+
+        when(purchaseRepository.findById(1L)).thenReturn(Optional.empty());
+
+        // Act & Assert
+        assertThrows(NotFoundException.class, () -> bookService.returnBook(request));
+
+        // Verify
+        verify(purchaseRepository, times(1)).findById(1L);
+        verifyNoInteractions(bookRepository);
+        verify(purchaseRepository, never()).save(any(Purchase.class));
+    }
+
+    // POST test
+    // given an invalid book ID, return book should throw NotFoundException
+    // current database design has a constraint: on delete restrict
+    // on foreign key book id in purchases table
+    // theoretically, book not found will not happen,
+    // service still get book not found gated to prevent potential error
+    @Test
+    void returnBookShouldThrowsNotFoundExceptionsGivenNonExistingBookID() {
+        // Assign
+        // create returnBook argument
+        ReturnBookRequestDTO request = new ReturnBookRequestDTO();
+        request.setPurchaseId(1L);
+
+        Book potentialBook = new Book();
+        potentialBook.setId(10L);
+
+        Purchase foundPurchase = new Purchase();
+        foundPurchase.setId(1L);
+        foundPurchase.setBook(potentialBook);
+
+        when(purchaseRepository.findById(1L)).thenReturn(Optional.of(foundPurchase));
+        when(bookRepository.findById(10L)).thenReturn(Optional.empty());
+
+        // Act & Assert
+        Exception exception = assertThrows(NotFoundException.class, () -> bookService.returnBook(request));
+        assertEquals("ReturnBook--book not found for with id: 10", exception.getMessage());
+
+        // Verify
+        verify(purchaseRepository, times(1)).findById(1L);
+        verify(bookRepository, times(1)).findById(10L);
+        verify(bookRepository,never()).save(any(Book.class));
+        verify(purchaseRepository,never()).save(any(Purchase.class));
+    }
+
+    // POST test
+    // given a purchase that already got book return processed,
+    // return book should throw DuplicateOperationException
+    // all process rolls back, no entity changes
+    @Test
+    void returnBookShouldThrowDuplicateOperationExceptionWhenAlreadyReturned() {
+        // Assign
+        // create returnBook argument
+        ReturnBookRequestDTO request = new ReturnBookRequestDTO();
+        request.setPurchaseId(1L);
+
+        Purchase foundPurchase = new Purchase();
+        foundPurchase.setId(1L);
+        foundPurchase.setStatus(PurchaseStatus.RETURN);
+
+        when(purchaseRepository.findById(1L)).thenReturn(Optional.of(foundPurchase));
+
+        // Act & Assert
+        Exception exception = assertThrows(DuplicateOperationException.class, () -> bookService.returnBook(request));
+        assertEquals("Books of purchase id: 1 has already been restocked", exception.getMessage());
+
+        // Verify
+        verify(purchaseRepository, times(1)).findById(1L);
+        verifyNoInteractions(bookRepository);
+        verify(purchaseRepository, never()).save(any(Purchase.class));
+    }
+
+    // POST test
+    // when fails to save updated purchase
+    // return purchase should throw DatabaseException
+    @Test
+    void returnBookShouldThrowDatabaseExceptionWhenFailsToSavePurchase() {
+        // Assign
+        // create returnBook argument
+        ReturnBookRequestDTO request = new ReturnBookRequestDTO();
+        request.setPurchaseId(1L);
+
+        // create book entity the repo returns when findById
+        Book foundBook = new Book();
+        foundBook.setId(10L);
+        foundBook.setStock(95);
+
+        // create purchase entity the repo returns when findById
+        Purchase foundPurchase = new Purchase();
+        foundPurchase.setId(1L);
+        foundPurchase.setQuantity(5);
+        foundPurchase.setBook(foundBook);
+        foundPurchase.setStatus(PurchaseStatus.CONFIRMED);
+
+        // create book entity the repo returns when save() is called
+        Book updatedBook = new Book();
+        updatedBook.setId(10L);
+        updatedBook.setStock(100);
+        updatedBook.setTitle("Title");
+        updatedBook.setAuthor("Author");
+
+        // create customer entity used in the returned purchase entity after save() via purchaseRepository
+        Customer customer = new Customer();
+        customer.setId(100L);
+
+        // create purchase entity the repo returns when save() is called
+        Purchase updatedPurchase = new Purchase();
+        updatedPurchase.setId(1L);
+        updatedPurchase.setQuantity(5);
+        updatedPurchase.setCustomer(customer);
+        updatedPurchase.setBook(foundBook);
+        updatedPurchase.setStatus(PurchaseStatus.RETURN);
+
+        when(purchaseRepository.findById(1L)).thenReturn(Optional.of(foundPurchase));
+        when(bookRepository.findById(10L)).thenReturn(Optional.of(foundBook));
+        when(purchaseRepository.save(any(Purchase.class))).thenThrow(new RuntimeException());
+
+        // Act & Assert
+        assertThrows(DatabaseException.class, () -> bookService.returnBook(request));
+
+        // Verify
+        verify(purchaseRepository,times(1)).findById(1L);
+        verify(bookRepository,times(1)).findById(10L);
+        // the real "no rollback needed because we never got there" check
+        verify(purchaseRepository, times(1)).save(any(Purchase.class));
     }
 
 }
