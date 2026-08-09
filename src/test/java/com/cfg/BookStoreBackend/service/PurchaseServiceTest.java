96a7185aad106808abff54c637e28fce1e3b87b7
Alicexiaoshuokeizer <xiaoshuoye@outlook.com>
2026-08-09 02:09:11 +0100

    Test: add unit test for `makePurchase()` in PurchaseService

--- src/test/java/com/cfg/BookStoreBackend/service/PurchaseServiceTest.java
+++ src/test/java/com/cfg/BookStoreBackend/service/PurchaseServiceTest.java
@@ -3,11 +3,14 @@ package com.cfg.BookStoreBackend.service;
 import com.cfg.BookStoreBackend.exception.DatabaseException;
 import com.cfg.BookStoreBackend.exception.DuplicateOperationException;
 import com.cfg.BookStoreBackend.exception.NotFoundException;
+import com.cfg.BookStoreBackend.exception.OutOfStockException;
+import com.cfg.BookStoreBackend.model.dto.PurchaseRequestDTO;
 import com.cfg.BookStoreBackend.model.dto.PurchaseResponseDTO;
 import com.cfg.BookStoreBackend.model.entity.Book;
 import com.cfg.BookStoreBackend.model.entity.Customer;
 import com.cfg.BookStoreBackend.model.entity.Purchase;
 import com.cfg.BookStoreBackend.model.repository.BookRepository;
+import com.cfg.BookStoreBackend.model.repository.CustomerRepository;
 import com.cfg.BookStoreBackend.model.repository.PurchaseRepository;
 import com.cfg.BookStoreBackend.util.PurchaseStatus;
 import org.junit.jupiter.api.Test;
@@ -17,10 +20,11 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
+//import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
@@ -32,10 +36,133 @@ class PurchaseServiceTest {
     private PurchaseRepository purchaseRepository;
 
     @Mock
+    private CustomerRepository customerRepository;
+
+    @Mock
     private BookRepository bookRepository;
 
     @InjectMocks
     private PurchaseService purchaseService;
+
+    // MAKE PURCHASE test, processing a valid purchase request
+    // should reduce book stock and return PurchaseResponseDTO with CONFIRMED status and price info
+    @Test
+    void makePurchaseShouldReduceBookStockAndReturnCorrectPurchaseStatusAndPriceInfoWhenValid() {
+        // Arrange
+        PurchaseRequestDTO request = new PurchaseRequestDTO();
+        request.setBookId(10L);
+        request.setCustomerId(15L);
+        request.setQuantity(5);
+
+        Book book = new Book();
+        book.setId(10L);
+        book.setStock(20); // stock > order quantity
+        book.setPrice(1.0);
+        book.setAuthor("Test Author");
+        book.setTitle("Test Title");
+
+        Customer customer = new Customer();
+        customer.setId(15L);
+
+        Purchase purchase = new Purchase(
+                book,
+                customer,
+                request.getQuantity(),
+                BigDecimal.valueOf(book.getPrice() * request.getQuantity()),
+                LocalDateTime.now(),
+                PurchaseStatus.CONFIRMED
+        );
+
+        when(customerRepository.findById(15L)).thenReturn(Optional.of(customer));
+        when(bookRepository.findById(10L)).thenReturn(Optional.of(book));
+        when(purchaseRepository.save(any(Purchase.class))).thenReturn(purchase);
+
+        // Act
+        PurchaseResponseDTO response = purchaseService.makePurchase(request);
+
+        // Assert
+        assertEquals(PurchaseStatus.CONFIRMED, response.getStatus());
+        assertEquals(BigDecimal.valueOf(5.0), response.getTransactionPrice());
+        assertEquals(15L, response.getCustomerId());
+        assertEquals(10L, response.getBookId());
+        // book stock reduces accordingly
+        assertEquals(15, book.getStock());
+
+        verify(customerRepository,times(1)).findById(15L);
+        verify(bookRepository, times(1)).findById(10L);
+        verify(purchaseRepository, times(1)).save(any(Purchase.class));
+    }
+
+    // MAKE PURCHASE test, processing an invalid purchase request with not-existing customer ID
+    // should throw NotFoundException
+    @Test
+    void makePurchaseShouldThrowNotFoundWhenCustomerIdIsNotFound() {
+        // Arrange
+        PurchaseRequestDTO request = new PurchaseRequestDTO();
+        request.setCustomerId(999L);
+
+        when(customerRepository.findById(999L)).thenReturn(Optional.empty());
+
+        // Assert
+        assertThrows(NotFoundException.class, () -> purchaseService.makePurchase(request));
+
+        verify(customerRepository,times(1)).findById(999L);
+        verify(bookRepository, never()).findById(10L);
+        verify(purchaseRepository, never()).save(any(Purchase.class));
+    }
+
+    // MAKE PURCHASE test, processing an invalid purchase request with not-existing book ID
+    // should throw NotFoundException
+
+    @Test
+    void makePurchaseShouldThrowNotFoundWhenBookIdIsNotFound() {
+        // Arrange
+        PurchaseRequestDTO request = new PurchaseRequestDTO();
+        request.setCustomerId(1L);
+        request.setBookId(1000L);
+
+        Customer customer = new Customer();
+
+        when(customerRepository.findById(1L)).thenReturn(Optional.of(customer));
+        when(bookRepository.findById(1000L)).thenReturn(Optional.empty());
+
+        // Assert
+        assertThrows(NotFoundException.class, () -> purchaseService.makePurchase(request));
+
+        verify(customerRepository, times(1)).findById(1L);
+        verify(bookRepository,times(1)).findById(1000L);
+        verify(purchaseRepository, never()).save(any(Purchase.class));
+    }
+
+    // MAKE PURCHASE test, processing an invalid purchase request
+    // with an order quantity > book stock
+    // should throw OutOfStockException
+    @Test
+    void makePurchaseShouldThrowOutOfStockExceptionWhenInsufficientStock() {
+        // Arrange
+        PurchaseRequestDTO request = new PurchaseRequestDTO();
+        request.setBookId(10L);
+        request.setCustomerId(15L);
+        request.setQuantity(50);
+
+        Book book = new Book();
+        book.setId(10L);
+        book.setStock(20); // stock > order quantity
+        book.setTitle("Title");
+
+        Customer customer = new Customer();
+        customer.setId(15L);
+
+        when(customerRepository.findById(15L)).thenReturn(Optional.of(customer));
+        when(bookRepository.findById(10L)).thenReturn(Optional.of(book));
+
+        // Assert: throws OutOfStockException and book stock has not changed as process rolls back
+        assertThrows(OutOfStockException.class, () -> purchaseService.makePurchase(request));
+        assertEquals(20, book.getStock());
+
+        verify(customerRepository,times(1)).findById(15L);
+        verify(bookRepository, times(1)).findById(10L);
+    }
 
     // REFUND test, processing a valid refund should change order status
     @Test
