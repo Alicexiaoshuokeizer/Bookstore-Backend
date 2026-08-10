3c07644da8d00ae4728970ee3638de3e8ebb374a
Alicexiaoshuokeizer <xiaoshuoye@outlook.com>
2026-08-10 20:31:54 +0100

    remove unused import from PurchaseServiceTest

--- src/test/java/com/cfg/BookStoreBackend/service/PurchaseServiceTest.java
+++ src/test/java/com/cfg/BookStoreBackend/service/PurchaseServiceTest.java
@@ -24,7 +24,6 @@ import java.time.LocalDateTime;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
-//import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
